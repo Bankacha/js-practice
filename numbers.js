c76d2b67d6e4acf5ccdba540f19b33c1625120ec
Bankacha <bgbrankoman@gmail.com>
2020-09-03 18:12:47 +0200

    numbers and students resolved

--- numbers.js
+++ numbers.js
@@ -1,20 +1,44 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Example of for loop to use instead of using forEach (remove this when start):
-for (let num of array) {
-    console.log(num);
-}
+// for (let num of array) {
+//    console.log(num);
+// }
 
 // 1.  Write code that calculates and print a sum of array
+var item = 0;
 
+for (let num of array) {
+    item = item + num;
+}
+//console.log(item)
 
 // 2.  Write code that calculates and print the average value of array
+var item2 = 0;
+for (let num of array) {
+    item2 = item2 + num;
+}
+avrgValArr = item2 / 2;
 
-
+//console.log(avrgValArr)
 // 3.  * Write code that builds new reverse array (see https://www.w3schools.com/js/js_loop_for.asp)
+let newArr = [];
+
+for (let i = array.length - 1; i >= 0; i--) {
+    const num = array[i];
+    newArr.push(num)
+}
+//console.log(newArr)
 
 
 // 4.  * Write code that prints the middle element if exist and null if not
+
+if (array.length % 2 > 0) {
+    const middleIndex = Math.floor(array.length / 2);
+    console.log(array[middleIndex])
+} else {
+    console.log(null)
+}
 
 
 
--- students.js
+++ students.js
@@ -32,35 +32,109 @@ const students = [
 ];
 
 // Example of for loop to use instead of using forEach (remove this when start):
-for (let student of students) {
-    console.log(student);
-}
+// for (let student of students) {
+//     console.log(student);
+// }
 
 
 // Write code for:
 
 //  1. Calculation and print of average rank of all students
+var ranks = 0;
 
-
+for (let student of students) {
+    ranks = ranks + student.rank;
+    avrgRank = ranks / students.length;
+}
+//console.log(avrgRank)
 
 //  2. Calculation and print average rank of female students
 
+var femaleRanks = 0;
+var numOfFemale = 0;
 
+for (let student of students) {
+    if (student.genders === "female") {
+        femaleRanks = femaleRanks + student.rank;
+        numOfFemale += 1;
+    }
+}
+//console.log(femaleRanks/numOfFemale)
+
+//or
+
+const avrgFemaleRank = femaleRanks / numOfFemale;
+
+//console.log(avrgFemaleRank)
 
 //  4. Calculation and print of sum of male students ranks
+sumOfFemaleRanks = 0;
 
-
+for (let student of students) {
+    if (student.genders === "female") {
+        sumOfFemaleRanks = sumOfFemaleRanks + student.rank;
+    }
+}
+//console.log(sumOfFemaleRanks)    
 
 //  5. Building two new arrays, one with male students, another with female
+let maleStudentsArr = [];
+let femaleStudentsArr = [];
 
-
+for (let student of students) {
+    if (student.genders === "male") {
+        maleStudentsArr.push(student);
+    } else {
+        femaleStudentsArr.push(student);
+    }
+}
+//console.log(maleStudentsArr);
+//console.log(femaleStudentsArr);
 
 //  6. Finding the student who has the biggest rank
+let biggestStudentRank = students[0];
 
-
+for (let student of students) {
+    if (biggestStudentRank.rank < student.rank) {
+        biggestStudentRank = student;
+    }
+}
+//console.log(biggestStudentRank)
 
 //  7. Finding the female student who has the smallest rank
+let femaleSmallestRanked = students[0];
 
-
+for (let student of students) {
+    if (students.gender === "female") {
+        if (!femaleSmallestRanked) {
+            femaleSmallestRanked = students;
+        } else {
+            if (femaleSmallestRanked.rank > student.rank) {
+                femaleSmallestRanked = student;
+            }
+        }
+    }
+}
+//console.log(femaleSmallestRanked)
 
 // 8. Calculation and print average rank of students who has rank over 7
+
+var studentsArrOver7 = [];
+
+for (let student of students) {
+    if (student.rank > 7) {
+        studentsArrOver7.push(student)
+    }
+}
+console.log(studentsArrOver7)
+
+let overSeven = 0;
+
+for (let student of studentsArrOver7) {
+    overSeven = overSeven + student.rank;
+}
+console.log(overSeven)
+
+let averageGradeOver7 = overSeven / studentsArrOver7.length;
+
+console.log(averageGradeOver7)
